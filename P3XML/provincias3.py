8404e33a1ff7463e45a095a9ab0b4598ca1de1b6
Alejandro López <alexlpinformatica@gmail.com>
2018-02-14 08:52:49 +0100

    Corregido orden de Provincias3

--- P3XML/provincias3.py
+++ P3XML/provincias3.py
@@ -3,11 +3,10 @@ doc = etree.parse('provinciasypoblaciones.xml')
 #print (etree.tostring(doc))
 raiz=doc.getroot()
 
-
+provincias=doc.findall("provincia")
+for provincia in provincias:
+	print (provincia[0].text)
 
 localidades=doc.findall("provincia/localidades/localidad")
 print (len(localidades))
 
-provincias=doc.findall("provincia")
-for provincia in provincias:
-	print (provincia[0].text)
