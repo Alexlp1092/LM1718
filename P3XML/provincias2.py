ee61f13ad0650235e6343d1d982b8df2ea225d71
Alejandro López <alexlpinformatica@gmail.com>
2018-02-23 10:54:28 +0100

    Metidos en carpeta

--- P3XML/provincias2.py
+++
@@ -1,8 +0,0 @@
-from lxml import etree
-doc = etree.parse('provinciasypoblaciones.xml')
-#print (etree.tostring(doc))
-raiz=doc.getroot()
-
-localidades=doc.findall("provincia/localidades/localidad")
-for localidad in localidades:
-	print(localidad.text)
